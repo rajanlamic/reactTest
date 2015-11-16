1c6aa79da9c3de5570b18e64d248045a3f1ae880
Rajan Lamichhane <rajanlamic@gmail.com>
2015-11-16 17:17:48 +0000

    initial

---
+++ WithNode/Gulpfile.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Rajan on 11/11/2015.
+ */
+
+"use strict";
+
+var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
+var babel = require("gulp-babel");
+var concat = require("gulp-concat");
+var browserify = require("gulp-browserify");
+
+
+gulp.task("build", function() {
+    return gulp.src("src/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            "presets": ["react"]
+        }))
+        .pipe(concat("all.js"))
+        .pipe(browserify(
+            {
+                debug: true
+            }
+        ))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task('watch', ['build'], function() {
+    gulp.watch('src/*.js', ['build']);
+});
+
+gulp.task('default', ['watch']);
